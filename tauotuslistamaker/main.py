2ce52bfb71c9f1d30794a9addb7bd6eb028e8881
= <nikolas.syverod@gmail.com>
2025-01-12 18:34:12 +0200

    Create algorithm for creating seating arrangement, have to clean the mess soon

--- tauotuslistamaker/main.py
+++ tauotuslistamaker/main.py
@@ -1,6 +1,7 @@
 import json
 from cashier import calculate_optimal_break_times, turn_cashiers_shift_start_and_end_times_to_datetime
 from tauotuslista import create_breaks_list, assign_tauottajat, create_seating_arrangement, get_cashiers_availability
+import copy
 
 
 def main():
@@ -9,6 +10,7 @@ def main():
         with open("cashiers.json", "r") as json_file:
             cashiers = turn_cashiers_shift_start_and_end_times_to_datetime(
                 json.load(json_file))
+            cashiers_shift_start_and_end_times = copy.deepcopy(cashiers)
     except FileNotFoundError:
         print("cashiers.json not found")
         return
@@ -38,7 +40,31 @@ def main():
     cashiers_availability = get_cashiers_availability(
         cashiers, breaks_list_with_assigned_tauottajat)
     cashier_seating_arrangement = create_seating_arrangement(cashiers_availability,
-                                                             breaks_list_with_assigned_tauottajat, config["checkout_lanes"], config["most_popular_checkouts"], config["least_popular_checkouts"], config["self_service_checkouts"], config["extra_checkouts"])
+                                                           config["checkout_lanes_filling_order"], config["self_service_checkouts"])
+#what dis?
+"""    for lane in cashier_seating_arrangement:
+        sorted_cashiers = sorted(
+            lane["cashiers"], key=lambda x: x["assigned_time_range"]["start_time"])
+    for cashier in cashiers_availability:
+
+        for availability in cashier["availability"]:
+            start_time = availability["start_time"].strftime("%H:%M")
+            end_time = availability["end_time"].strftime("%H:%M")
+
+        for break_ in breaks_list_with_assigned_tauottajat:
+            for break_time in break_["breaks"]:
+                if break_time["name"] == cashier["name"]:
+                    start_time = break_time["start_time"].strftime("%H:%M")
+                    end_time = break_time["end_time"].strftime("%H:%M")
+
+        for lane in cashier_seating_arrangement:
+            if cashier["name"] in [c["name"] for c in lane["cashiers"]]:
+                for assigned_cashier in lane["cashiers"]:
+                    if assigned_cashier["name"] == cashier["name"]:
+                        start_time = assigned_cashier["assigned_time_range"]["start_time"].strftime(
+                            "%H:%M")
+                        end_time = assigned_cashier["assigned_time_range"]["end_time"].strftime(
+                            "%H:%M") """
 
 
 if __name__ == "__main__":
--- tauotuslistamaker/tauotuslista.py
+++ tauotuslistamaker/tauotuslista.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 import copy
 
 
@@ -155,17 +155,216 @@ def get_cashiers_availability(cashiers, breaks_list_with_assigned_tauottajat):
 
 
 def split_time_range(current_start_time_from, current_end_time_to, time_to_split_from, time_to_split_to, time_start_key, time_end_key):
-    if current_start_time_from <= time_to_split_from and current_end_time_to >= time_to_split_from:
+    if current_start_time_from < time_to_split_from and current_end_time_to > time_to_split_to:
+        print(f"Time {time_to_split_from} - {time_to_split_to} is within the time {current_start_time_from} - {current_end_time_to}\n")
         time_range_1 = {
             time_start_key: current_start_time_from, time_end_key: time_to_split_from}
         time_range_2 = {
             time_start_key: time_to_split_to, time_end_key: current_end_time_to}
+        print(f"Split time into two ranges:\n {time_range_1}\n {time_range_2}\n")
         return [time_range_1, time_range_2]
     elif time_to_split_from <= current_start_time_from and time_to_split_to < current_end_time_to:
+        print(f"Time {time_to_split_from} - {time_to_split_to} starts before or at the same time as the time {current_start_time_from} - {current_end_time_to} and ends within the time.\n")
         return [{time_start_key: time_to_split_to, time_end_key: current_end_time_to}]
     elif time_to_split_from > current_start_time_from and time_to_split_to >= current_end_time_to:
+        print(f"Time {time_to_split_from} - {time_to_split_to} starts within the time {current_start_time_from} - {current_end_time_to} and ends after or at the same time as the time.\n")
         return [{time_start_key: current_start_time_from, time_end_key: time_to_split_from}]
 
 
-def create_seating_arrangement(cashiers_availability, amount_of_checkout_lanes, most_popular_checkouts, least_popular_checkouts, self_service_checkouts, extra_checkouts):
-    pass
+def create_seating_arrangement(cashiers_availability, checkout_lanes_filling_order, self_service_checkouts):
+    # print each cashiers availability in the console with a normal time format
+    for cashier in cashiers_availability:
+        print("Cashier: ", cashier["name"])
+        for availability in cashier["availability"]:
+            print("Start: ", availability["start_time"].strftime(
+                "%H:%M"), "End: ", availability["end_time"].strftime("%H:%M"))
+    seating_arrangement = []
+
+    cashiers_availability_copy = copy.deepcopy(cashiers_availability)
+    all_checkouts = copy.deepcopy(self_service_checkouts) + copy.deepcopy(checkout_lanes_filling_order)
+    
+    for i, checkout in enumerate(all_checkouts):
+        print("Processing all checkouts:\n")
+        print(all_checkouts)
+        checkout_availability = {"start": None, "end": None}
+        if isinstance(checkout, dict):
+            checkout["availability"] = []
+            checkout_availability["start"] = datetime.strptime(checkout["opening_time"], "%H:%M")
+            checkout_availability["end"] = datetime.strptime(checkout["closing_time"], "%H:%M")
+            # Remove the opening_time and closing_time keys from the checkout dict
+            del checkout["opening_time"]
+            del checkout["closing_time"]
+        elif isinstance(checkout, int):
+            # Convert the integer to a dictionary with a default structure
+            all_checkouts[i] = {"name": f"Checkout {checkout}", "availability": []}
+            checkout = all_checkouts[i]
+            checkout_availability["start"] = datetime.min
+            checkout_availability["end"] = datetime.max
+        else:
+            # Handle other unexpected types if necessary
+            continue
+        
+        print(f"Initialized checkout:\n {checkout}\n")
+        checkout["availability"].append(checkout_availability)
+
+    while cashiers_availability_copy:
+        for checkout in all_checkouts:
+            checkout_assigned_cashiers = {"checkout": checkout["name"], "cashiers": []}
+            while checkout["availability"]:
+                print(f"Checkout full availability:\n {checkout['availability']}\n")
+                checkout_availability = checkout["availability"].pop(0)
+                
+
+                print(f"Processing checkout availability:\n {checkout_availability}\n")
+
+                print(f"searching suitable cashier for checkout {checkout['name']}...\n")
+                selected_cashier, selected_cashier_new_availability = find_cashier_to_fill_available_time_at_checkout_lane(
+                    cashiers_availability_copy, 
+                    checkout_availability["start"], 
+                    checkout_availability["end"]
+                )
+                if selected_cashier is None:
+                    print("No suitable cashier found for this availability.\n")
+                    continue
+                else:
+                    print(f"Selected cashier: {selected_cashier['name']} for checkout: {checkout['name']}\n")
+                    checkout_assigned_cashiers["cashiers"].append(selected_cashier)
+                    for cashier in cashiers_availability_copy:
+                        if cashier["name"] == selected_cashier["name"]:
+                            if selected_cashier_new_availability == []:
+                                cashiers_availability_copy.remove(cashier)
+                            else:
+                                cashier["availability"] = selected_cashier_new_availability
+                            break
+    
+                    if selected_cashier["assigned_time_range"]["start_time"] > checkout_availability["start"] or selected_cashier["assigned_time_range"]["end_time"] < checkout_availability["end"]:
+                        print(f"Selected cashier has a modified availability:\n {selected_cashier_new_availability}\n")
+                        checkout_modified_availability = split_time_range(
+                            checkout_availability["start"], 
+                            checkout_availability["end"], 
+                            selected_cashier["assigned_time_range"]["start_time"], 
+                            selected_cashier["assigned_time_range"]["end_time"], 
+                            time_start_key="start", 
+                            time_end_key="end"
+                        )
+                        print(f"Modified checkout availability from {checkout_availability['start']} - {checkout_availability['end']} because of cashier {selected_cashier['assigned_time_range']['start_time']} - {selected_cashier['assigned_time_range']['end_time']}:\n {checkout_modified_availability}\n")
+                        checkout["availability"].extend(checkout_modified_availability)
+                        print(f"Updated checkout availability:\n {checkout['availability']}\n")
+            print(f"Appending assigned cashiers {checkout_assigned_cashiers} for checkout {checkout['name']} to the seating arrangement.\n")
+            seating_arrangement.append(checkout_assigned_cashiers)
+            print(f"Cashiers availability after checkout {checkout['name']}:\n {cashiers_availability_copy}\n")
+            print(f"Assigned cashiers for checkout {checkout['name']}:\n {checkout_assigned_cashiers['cashiers']}\n")
+    return seating_arrangement
+
+def update_candidate(cashier, start_time, end_time, checkout_start, checkout_end):
+    new_availability = copy.deepcopy(cashier["availability"])
+    new_availability.remove({"start_time": start_time, "end_time": end_time})
+    # only split time ranges if the cashier's availability is not fully within the checkout availability time
+    if (start_time != checkout_start) or (end_time != checkout_end):
+        split_time_ranges = split_time_range(start_time, end_time, checkout_start, checkout_end, time_start_key="start_time", time_end_key="end_time")
+        new_availability.extend(split_time_ranges)
+    print(f"checkout unavailable time: {checkout_start} - {checkout_end}, cashier available time: {start_time} - {end_time}\n")
+    current_best_cashier_candidate = {
+        "name": cashier["name"],
+        "assigned_time_range": {
+            "start_time": checkout_start,
+            "end_time": checkout_end
+        }
+    }
+    print(f"Updated candidate:\n {current_best_cashier_candidate} with new availability:\n {new_availability}\n")
+    return current_best_cashier_candidate, new_availability
+
+def find_cashier_to_fill_available_time_at_checkout_lane(cashiers_availability, checkout_availability_start_time, checkout_availability_end_time):
+    current_best_cashier_candidate = {
+        "name": None,
+        "assigned_time_range": {
+            "start_time": None,
+            "end_time": None
+        }
+    }
+    new_availability = []
+
+    print("Starting search for best cashier candidate...\n")
+    print(f"Checkout availability start time: {checkout_availability_start_time}, end time: {checkout_availability_end_time}\n")
+
+    for cashier in cashiers_availability:
+        print(f"Checking cashier: {cashier['name']}\n")
+        for cashier_availability_time in cashier["availability"]:
+            print(f"  Cashier availability:\n {cashier_availability_time}\n")
+
+            if cashier_availability_time["start_time"] <= checkout_availability_start_time and cashier_availability_time["end_time"] >= checkout_availability_end_time:
+                print("  Found a perfect match for the entire checkout availability time.\n")
+                current_best_cashier_candidate, new_availability = update_candidate(
+                    cashier, cashier_availability_time["start_time"], cashier_availability_time["end_time"],
+                    checkout_availability_start_time, checkout_availability_end_time
+                )
+                print(f"  Selected cashier:\n {current_best_cashier_candidate}\n")
+                return current_best_cashier_candidate, new_availability
+
+            elif cashier_availability_time["start_time"] >= checkout_availability_start_time and cashier_availability_time["end_time"] <= checkout_availability_end_time:
+                print("  Cashier availability is within the checkout availability time.\n")
+                if current_best_cashier_candidate["assigned_time_range"]["start_time"] is None or (cashier_availability_time["end_time"] - cashier_availability_time["start_time"]).total_seconds() / 60 > (current_best_cashier_candidate["assigned_time_range"]["end_time"] - current_best_cashier_candidate["assigned_time_range"]["start_time"]).total_seconds() / 60:
+                    current_best_cashier_candidate, new_availability = update_candidate(
+                        cashier, cashier_availability_time["start_time"], cashier_availability_time["end_time"],
+                        cashier_availability_time["start_time"], cashier_availability_time["end_time"]
+                    )
+                    print(f"  Updated best candidate:\n {current_best_cashier_candidate}\n")
+                    continue
+
+            elif cashier_availability_time["start_time"] < checkout_availability_end_time and checkout_availability_start_time < cashier_availability_time["end_time"] < checkout_availability_end_time:
+                print("  Cashier availability overlaps with the start of the checkout availability time.\n")
+                if current_best_cashier_candidate["assigned_time_range"]["start_time"] is None or (cashier_availability_time["end_time"] - checkout_availability_start_time).total_seconds() / 60 > (current_best_cashier_candidate["assigned_time_range"]["end_time"] - current_best_cashier_candidate["assigned_time_range"]["start_time"]).total_seconds() / 60:
+                    current_best_cashier_candidate, new_availability = update_candidate(
+                        cashier, cashier_availability_time["start_time"], cashier_availability_time["end_time"],
+                        checkout_availability_start_time, cashier_availability_time["end_time"]
+                    )
+                    print(f"  Updated best candidate:\n {current_best_cashier_candidate}\n")
+                    continue
+
+            elif checkout_availability_start_time < cashier_availability_time["start_time"] < checkout_availability_end_time and cashier_availability_time["end_time"] > checkout_availability_end_time:
+                print("  Cashier availability overlaps with the end of the checkout availability time.\n")
+                if current_best_cashier_candidate["assigned_time_range"]["start_time"] is None or (checkout_availability_end_time - cashier_availability_time["start_time"]).total_seconds() / 60 > (current_best_cashier_candidate["assigned_time_range"]["end_time"] - current_best_cashier_candidate["assigned_time_range"]["start_time"]).total_seconds() / 60:
+                    current_best_cashier_candidate, new_availability = update_candidate(
+                        cashier, cashier_availability_time["start_time"], cashier_availability_time["end_time"],
+                        cashier_availability_time["start_time"], checkout_availability_end_time
+                    )
+                    print(f"  Updated best candidate:\n {current_best_cashier_candidate}\n")
+                    continue
+
+    if current_best_cashier_candidate["name"] is None:
+        print("No suitable cashier found.\n")
+        return None, None
+    else:
+        print(f"Best cashier candidate found:\n {current_best_cashier_candidate}\n")
+        return current_best_cashier_candidate, new_availability
+            
+
+def is_time_range_within(outer_time_range, inner_time_range, start_key="start_time", end_key="end_time"):
+    """
+    Check if the inner time range is fully within the outer time range.
+
+    Parameters:
+    - outer_time_range: Dictionary with the outer time range.
+    - inner_time_range: Dictionary with the inner time range.
+    - start_key: Key for the start time in the dictionaries.
+    - end_key: Key for the end time in the dictionaries.
+
+    Returns:
+    - Boolean indicating if the inner time range is fully within the outer time range.
+    """
+    return ( outer_time_range[start_key] <= inner_time_range[start_key] and
+            outer_time_range[end_key] >= inner_time_range[end_key])
+
+def get_time_range_duration(time_range, start_key="start_time", end_key="end_time"):
+    """
+    Get the duration of a time range in minutes.
+
+    Parameters:
+    - time_range: Dictionary with the time range.
+    - start_key: Key for the start time in the dictionary.
+    - end_key: Key for the end time in the dictionary.
+
+    Returns:
+    - Duration of the time range in minutes.
+    """
+    return (time_range[end_key] - time_range[start_key]).total_seconds() / 60 
